c61627e3da4d18de761d5b9c0af9a1769d0c6ee1
unghwi00 <dldbtkd3573@gmail.com>
2021-09-01 16:30:18 +0900

    i

---
+++ Project school/pro/gmae.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+//Game클래스 선언
+
+class Game {
+private:
+	double energy = 100;
+	static int life;
+	x;
+public:
+	Game();
+	double Damage();
+	static void Die();
+};
+int Game::life = 10;
+
+void Game::Die() {
+	life--;
+	cout << "**생명이 1 감소하여 " << life << "이 되었습니다." << endl << endl; 
+
+}
+double Game::Damage() {
+	double damaged = 0;
+
+	if (energy > 0) {
+		damaged += rand() % 100;
+		 energy -= damaged;
+		cout << "에너지가 " << damaged << " 감소하여 " << energy << " 이 되었습니다.\n";
+		if (energy < 0) {
+			life--;
+			energy = 100;
+			cout << "생명이 1 감소하여 " << life << "이 되었습니다." << endl << endl;
+
+		}
+	}
+	return energy;
+}
+//Game클래스 멤버 함수의 정의
+Game::Game()
+{
+	cout << "에너지가 " << energy << " 이며, 생명이 " << life << "인 캐릭터가 만들어졌습니다.\n";
+	Damage();
+	Damage();
+	Damage();
+	Damage();
+	Damage();
+
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL)); //seed값으로 현재시간 부여
+	Game c1;
+	Game::Die();
+	Game c2;
+	c2.Damage();
+	c2.Damage();
+	c2.Damage();
+	c2.Die();
+	return 0;
+}
