2861818422dc27e2b90dd363b7cb91bcc87c7a68
unghwi00 <dldbtkd3573@gmail.com>
2021-09-01 15:26:45 +0900

    i

--- Project school/Project1/main.cpp
+++ Project school/Project1/main.cpp
@@ -8,7 +8,7 @@ public:
 	static int top;
 	static string line[100];
 
-	static void add(string str);//¿Ö ¾ÈµÊ
+	static void add(string str);//gld
 	static void print();
 };
 string Board::line[] = { "", };
